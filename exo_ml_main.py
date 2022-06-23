86a4e5a693ba768fcd286c0f1190f9b804a2859b
Sophie Frankel <sophie.frankel814@gmail.com>
2022-06-23 16:57:10 +0100

    Added main file

---
+++ exo_ml_main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib as mpl
+import pandas as pd
+
+# db = pd.read_table(r"C:\Users\ItIsO\Documents\GitHub\AMATEUR_exoplanet_data\UID_0013192_data_AXA_002.tbl")
+
+# plot data
+
+# save plot as img
+
+# neural network to classify img
+
+# function to take in and classify (user-input) test data
