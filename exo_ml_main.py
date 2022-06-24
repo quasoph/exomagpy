d33bbca851cf34e5b950a2363ad71a7ea12c46e6
Sophie Frankel <sophie.frankel814@gmail.com>
2022-06-24 11:13:26 +0100

    Imported tensorflow and related packages

--- exo_ml_main.py
+++ exo_ml_main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import matplotlib as mpl
 import pandas as pd
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
 # db = pd.read_table(r"C:\Users\ItIsO\Documents\GitHub\AMATEUR_exoplanet_data\UID_0013192_data_AXA_002.tbl")
 
