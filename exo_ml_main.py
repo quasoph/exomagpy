c0880a24ebfa8dd0d9227f0a6e59ed44a9ea2221
Sophie Frankel <sophie.frankel814@gmail.com>
2022-06-24 15:03:11 +0100

    Completed CNN model, trained and predicted data.

--- exo_ml_main.py
+++ exo_ml_main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
 import lightkurve
@@ -11,8 +11,9 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 
 train_df_true = pd.read_table(r"C:\Users\ItIsO\Documents\GitHub\AMATEUR_exoplanet_data\UID_0013192_data_AXA_002.tbl") # has exoplanets
-
 train_df_false = pd.read_table(r"C:\Users\ItIsO\Documents\GitHub\AMATEUR_exoplanet_data\UID_0013192_data_AXA_002.tbl") # has no exoplanets
+
+test_df = input("Enter your data: ")
 
 ls_imgs_true = []
 ls_imgs_false = []
@@ -40,6 +41,13 @@ train_ds = train.flow_from_directory(
     batch_size = 32,
     class_mode = 'binary')
 
+test_ds = test.flow_from_directory(
+    "IMAGE FOLDER PATH",
+    target_size=(150,150),
+    batch_size = 32,
+    class_mode = 'binary')
+
+
 # BUILD CNN MODEL
 
 model = keras.Sequential()
@@ -61,6 +69,34 @@ model.add(keras.layers.Flatten())
 model.add(keras.layers.Dense(512,activation="relu"))
 
 model.add(keras.layers.Dense(1,activation="sigmoid"))
+
+model.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
+
+# TRAIN DATA
+
+model.fit_generator(
+    train_ds,
+    steps_per_epoch = 250,
+    epochs = 10,
+    validation_data = test_ds
+)
+
+# PREDICTIONS (FUNCTION)
+
+def predictExo(filename):
+    img = image.load_img(filename,target_size=(150,150))
+    plt.imshow(img)
+
+    Y = image.img_to_array(img)
+    X = np.expand_dims(Y,axis=0)
+
+    val = model.predict(X)
+    print(val)
+
+    if val == 1:
+        plt.xlabel("Exoplanet detected!",fontsize=30)
+    elif val == 0:
+        plt.xlabel("No exoplanet detected.",fontsize=30)
 
 
 
