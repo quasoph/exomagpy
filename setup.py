f0c4719fbaac393d3b86c422d8ce63b820ab8cf6
Sophie Frankel <sophie.frankel814@gmail.com>
2022-07-01 17:41:34 +0100

    Added more info to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name = "exomagpy",
     version = "0.1",
+    author = "Sophie Frankel",
+    author_email = "s.frankel2@newcastle.ac.uk",
+    url = "https://github.com/quasoph/exomagpy",
+    description = "EXOMAGPY - EXOplanet MAchine learninG PYthon. A python package to find exoplanets using a binary image classification neural network. Looks at shiny things!",
     packages = find_packages()
 )
