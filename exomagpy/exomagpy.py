fde07961f1e045e2b5c41e00e011abcf62fb7f97
Sophie Frankel <sophie.frankel814@gmail.com>
2022-07-03 12:09:36 +0100

    Removed plot directories

--- exomagpy/exomagpy.py
+++ exomagpy/exomagpy.py
@@ -4,25 +4,16 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
-import tensorflow as tf
+import tensorflow
 import lightkurve as lk
-import io
+
+from PIL import Image
 
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
-
-import astropy
-from astropy.timeseries import TimeSeries
-
-# FOLDERS AND DIRECTORIES
-
-lc_train_folder = "./lightcurvePLOTS/" # create one for lightcurves with no exoplanets
-lc_train_dir = os.listdir(lc_train_folder)
-lc_exo_plots = "./lightcurvePLOTS/has_exoplanets/"
-lc_fa_plots = "./lightcurvePLOTS/no_exoplanets/" # false alarms
 
 # PHASE AND BIN FUNCTION
 
@@ -31,156 +22,110 @@ def phase_and_bin(data,period,t0,bin_time):
     data_binned = data_phased.bin(bin_time)
     return data_binned
 
-# PLOT AND SAVE FUNCTION
+# CONVERT PLOT TO ARRAY FUNCTION
 
-def plot_and_save(pltdata,path,name,num):
-    pltdata.plot(linewidth=0,marker=".")
-    plt.savefig(os.path.abspath(path) + name + str(num) + ".png")
-    plt.show()
-    return
+def plot_to_array(pltdata):
 
-# FETCH DATA (TESS DATA W/ LIGHTKURVE)
-
-exonames_tbl = pd.read_csv(os.path.abspath("PS_2022.06.27_08.12.38.csv"),delimiter=",",comment="#")
-TICs = exonames_tbl["tic_id"]
-periods = exonames_tbl["pl_orbper"] # for phasing
-epochs = exonames_tbl["pl_orbtper"] # for phasing
-
-bintime = 15/24/60
-
-
-for x in range(0,17):
-    print(TICs[x])
-    name = TICs[x]
-    if np.isnan(periods[x]) == False and np.isnan(epochs[x]) == False:
-        train_exo_search = lk.search_lightcurve(str(name),author="SPOC")[0:5]
-        lc_exo_train = train_exo_search.download_all().stitch()
-        
-        lc_exo_train_binned = phase_and_bin(lc_exo_train,periods[x],epochs[x],bintime)
-        
-        plot_and_save(lc_exo_train_binned,lc_exo_plots,"/lc_exo_trainplots_",x)
-
-fa_tbl = pd.read_csv(os.path.abspath("TOI_2022.06.29_08.07.35.csv"),delimiter=",",comment="#")
-fa_TICs = fa_tbl["tid"]
-
-y = 0
-
-while y < len(fa_TICs):
-    name1 = fa_TICs[y]
-    try:
-        train_fa_search = lk.search_lightcurve("TIC " + str(name1),author="SPOC",sector=1)
-        lc_fa_train = train_fa_search.download()
-        plot_and_save(lc_fa_train,lc_fa_plots,"/lc_fa_trainplots_",y)
-    except:
-        train_fa_search = lk.search_lightcurve("TIC " + str(name1),author="TESS",sector=1)
-        lc_fa_train = train_fa_search.download()
-        plot_and_save(lc_fa_train,lc_fa_plots,"/lc_fa_trainplots_",y)
-    else:
-        print("No lightcurve found.")
+    lc = pltdata.plot(linewidth=0,marker=".")
+    img = np.asarray(bytearray(lc.read()),dtype=np.uint8)
     
-    y = y + 1
+    return img
 
-# USER INPUT TARGET STAR AND MISSION
+# GET ARRAY OF IMAGE ARRAYS FOR A FILE
 
-target = str(input("Enter your target: "))
-mission = str(input("Enter mission: "))
-quart = input("Enter quarter (Kepler) or sector (TESS), A for all, M for multiple: ")
+def get_lightcurves(filename):
 
-# FIND, PLOT AND SAVE TEST DATA
+    tbl = pd.read_csv(os.path.abspath(filename),delimiter=",",comment="#")
+    TICs = tbl["tid"]
 
-test_plot_dir = "./testPLOTS/"
+    x = 0
 
-if quart == "A" or quart == "a" or quart == "all" or quart == "All":
-    testsearch = lk.search_lightcurve(target,author=mission)
-    lc_test = testsearch.download()
+    imgarray = []
 
-elif quart == "M":
-    q1 = int(input("Enter first quarter/sector: "))
-    q2 = int(input("Enter last quarter/sector: "))
-    testsearch = lk.search_lightcurve(target,author=mission)[q1:q2]
-    lc_test = testsearch.download_all().stitch()
+    while x < len(TICs):
+        name = TICs[x]
+        try:
+            search = lk.search_lightcurve("TIC " + str(name),author="SPOC",sector=1)
+            lc = search.download()
+            array = plot_to_array(lc)
+            imgarray.append(array)
+        except:
+            search = lk.search_lightcurve("TIC " + str(name),author="TESS",sector=1)
+            lc = search.download()
+            array = plot_to_array(lc)
+            imgarray.append(array)
+        else:
+            print("No lightcurve found.")
+    
+        x = x + 1
 
-else:
-    if mission == "Kepler":
-        testsearch = lk.search_lightcurve(target,author=mission,quarter=int(quart))
-    elif mission == "SPOC":
-        testsearch = lk.search_lightcurve(target,author=mission,sector=int(quart))
-    lc_test = testsearch.download()
-
-period = 83.8979
-t0 = 2125.847
-
-lc_test_binned = phase_and_bin(lc_test,period,t0,bintime)
-
-plot_and_save(lc_test_binned,test_plot_dir,"/lc_testplot.png",0)
+        return imgarray
 
 # CREATE TRAIN AND TEST DATASETS
 
-train = ImageDataGenerator(rescale=1/255)
-test = ImageDataGenerator(rescale=1/255)
-batchsize = 7
-trainplots = "./lightcurvePLOTS/"
+def predictExo(exotrainfile,noexotrainfile,testfile):
 
-train_ds = train.flow_from_directory(
-    os.path.abspath(trainplots),
-    target_size=(150,150),
-    batch_size = batchsize,
-    class_mode = 'binary')
+    train = ImageDataGenerator(rescale=1/255)
+    test = ImageDataGenerator(rescale=1/255)
+    batchsize = 7
 
-test_ds = test.flow_from_directory(
-    os.path.abspath(test_plot_dir),
-    target_size=(150,150),
-    batch_size = batchsize,
-    class_mode = 'binary')
+    train_ds = train.flow(
+        get_lightcurves(exotrainfile),
+        get_lightcurves(noexotrainfile),
+        target_size=(150,150),
+        batch_size = batchsize,
+        class_mode = 'binary')
 
-# BUILD CNN MODEL
+    test_ds = test.flow(
+        get_lightcurves(testfile),
+        target_size=(150,150),
+        batch_size = batchsize,
+        class_mode = 'binary')
 
-model = keras.Sequential()
+    # BUILD CNN MODEL
 
-model.add(keras.layers.Conv2D(32,(3,3),activation="relu",input_shape=(150,150,3)))
-model.add(keras.layers.MaxPool2D(2,2))
+    model = keras.Sequential()
 
-model.add(keras.layers.Conv2D(64,(3,3),activation="relu"))
-model.add(keras.layers.MaxPool2D(2,2))
+    model.add(keras.layers.Conv2D(32,(3,3),activation="relu",input_shape=(150,150,3)))
+    model.add(keras.layers.MaxPool2D(2,2))
 
-model.add(keras.layers.Conv2D(128,(3,3),activation="relu"))
-model.add(keras.layers.MaxPool2D(2,2))
+    model.add(keras.layers.Conv2D(64,(3,3),activation="relu"))
+    model.add(keras.layers.MaxPool2D(2,2))
 
-model.add(keras.layers.Conv2D(128,(3,3),activation="relu"))
-model.add(keras.layers.MaxPool2D(2,2))
+    model.add(keras.layers.Conv2D(128,(3,3),activation="relu"))
+    model.add(keras.layers.MaxPool2D(2,2))
 
-model.add(keras.layers.Flatten())
+    model.add(keras.layers.Conv2D(128,(3,3),activation="relu"))
+    model.add(keras.layers.MaxPool2D(2,2))
 
-model.add(keras.layers.Dense(512,activation="relu"))
+    model.add(keras.layers.Flatten())
 
-model.add(keras.layers.Dense(1,activation="sigmoid"))
+    model.add(keras.layers.Dense(512,activation="relu"))
 
-model.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
+    model.add(keras.layers.Dense(1,activation="sigmoid"))
 
-# TRAIN DATA
+    model.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
 
-model.fit(
-    train_ds,
-    steps_per_epoch = 250,
-    epochs = 10,
-    validation_data = test_ds
-)
+    # TRAIN DATA
 
-# PREDICTIONS (FUNCTION)
+    model.fit(
+        train_ds,
+        steps_per_epoch = 250,
+        epochs = 10,
+        validation_data = test_ds
+    )
 
-def predictExo(filename):
-    img = image.load_img(filename,target_size=(150,150))
-    plt.imshow(img)
+    for y in testfile:
 
-    Y = image.img_to_array(img)
-    X = np.expand_dims(Y,axis=0)
+        Y = get_lightcurves(testfile[y])
+        pic = Image.fromarray(Y)
+        pic.show()
+        X = np.expand_dims(Y,axis=0)
 
-    val = model.predict(X)
-    print(val)
+        val = model.predict(X)
+        print(val)
 
-    if val == 1:
-        plt.xlabel("Exoplanet detected!",fontsize=30)
-    elif val == 0:
-        plt.xlabel("No exoplanet detected.",fontsize=30)
-
-predictExo(os.path.join(os.path.abspath(test_plot_dir),"valid_exoplanets","lc_testplot.png"))
+        if val == 1:
+            plt.xlabel("Exoplanet detected!",fontsize=30)
+        elif val == 0:
+            plt.xlabel("No exoplanet detected.",fontsize=30)
