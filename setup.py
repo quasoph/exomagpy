ac46283af4e3fd4e404db402bb74ff22460eba99
Sophie Frankel <sophie.frankel814@gmail.com>
2022-07-14 14:34:03 +0100

    Stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "exomagpy",
-    version = "1.0.0",
+    version = "1.0.1",
     author = "Sophie Frankel",
     author_email = "s.frankel2@newcastle.ac.uk",
     url = "https://github.com/quasoph/exomagpy",
