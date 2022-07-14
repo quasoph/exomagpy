afb5eab9d3066c8eb8eca4a9b551e2eedc03e97c
Sophie Frankel <sophie.frankel814@gmail.com>
2022-07-14 14:56:13 +0100

    fixed

--- exomagpy/get_lightcurves.py
+++ exomagpy/get_lightcurves.py
@@ -13,6 +13,26 @@ from tensorflow import keras
 
 warnings.filterwarnings("ignore")
 
+def download(search):
+
+    lc = search.download()
+    
+    if lc is not None:
+        
+        fig,ax = plt.subplots()
+        ax.scatter(lc.time.value.tolist(), lc.flux.value.tolist(),s=0.1, color='k')
+        ax.autoscale()
+        ax.set_xlabel('Time (BTJD)')
+        ax.set_ylabel('Flux')
+        plt.close(fig)
+        io_buf = io.BytesIO()
+        fig.savefig(io_buf,format="raw")
+        io_buf.seek(0)
+        img_arr = (np.frombuffer(io_buf.getvalue(),dtype=np.uint8)).reshape(288,-1)
+        io_buf.close()
+
+        return img_arr
+
 def get_lightcurves(filename,length):
 
     tbl = pd.read_csv(os.path.abspath(filename),delimiter=",",comment="#")
